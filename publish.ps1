98df1a3677b575d42a77a2aaa74238da713d49e8
Emil Petro <petroemil@live.com>
2019-05-18 13:46:29 +0100

    [Source/Publish] Added a powershell script to help with the publish process

---
+++ publish.ps1
@@ -0,0 +1,16 @@
+# Create the release
+# dotnet publish -c Release
+
+# Copying the published content to the root (on GitHub Pages, that's publishing)
+$rootPath = ".\"
+$publishPath = ".\source\BlazorSite\bin\Release\netstandard2.0\publish\BlazorSite\dist"
+
+$ignored_root = @(".git", "source", "content", ".gitattributes", ".gitignore", ".nojekyll", "README.md", "publish.ps1")
+$ignored_published = @("content")
+
+Get-ChildItem -Exclude $ignored_root | Remove-Item -Recurse
+Get-ChildItem $publishPath -Exclude $ignored_published | Copy-Item -Destination $rootPath -Recurse
+
+# Publish release
+# git commit -m "[Publish]"
+# git push
