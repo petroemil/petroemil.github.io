0ee0ca546482c0a31d4b859287eded5727c5986e
Emil Petro <petroemil@live.com>
2019-05-18 17:38:26 +0100

    [Publish]

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
 # Create the release
-# dotnet publish -c Release
+dotnet publish .\source -c Release
 
 # Copying the published content to the root (on GitHub Pages, that's publishing)
 $rootPath = ".\"
@@ -12,5 +12,5 @@ Get-ChildItem -Exclude $ignored_root | Remove-Item -Recurse
 Get-ChildItem $publishPath -Exclude $ignored_published | Copy-Item -Destination $rootPath -Recurse
 
 # Publish release
-# git commit -m "[Publish]"
-# git push
+git commit -m "[Publish]"
+git push
