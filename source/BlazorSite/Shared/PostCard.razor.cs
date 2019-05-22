fdb0cfb95eebbcb8a48bcd7a5c0bd204c92b9724
Emil Petro <petroemil@live.com>
2019-05-22 22:45:39 +0100

    [Source] Simplified the usage of "PostCards" into a single component

---
+++ source/BlazorSite/Shared/PostCard.razor.cs
@@ -0,0 +1,14 @@
+﻿using BlazorSite.BlogService;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorSite.Shared
+{
+    public class PostCardBase : ComponentBase
+    {
+        [Parameter]
+        protected BlogPostDetails PostDetails { get; set; }
+
+        [Parameter]
+        protected bool IsDetailedView { get; set; }
+    }
+}
